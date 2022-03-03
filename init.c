1304180c0d208206e65158dd6aad6121ade82166
Josuah Demangeon <me@josuah.net>
2022-03-03 23:35:26 +0100

    flatten the directory further

--- init.c
+++ init.c
@@ -7,36 +7,52 @@ extern char __data_start, __data_end, __data_load_start;
 extern char __bss_start, __bss_end, __stack_top;
 
 void *__stack_pointer = &__stack_top;	/* 0x00 */
+
 void (*__vectors[])(void) = {
-	&__reset_handler,		/* 0x04 */
-	&__stop_program,		/* 0x08 */
-	&__stop_program,		/* 0x0C: hardfault */
-	&__stop_program,		/* 0x10: memorymanagement */
-	&__stop_program,		/* 0x14: busfault */
-	&__stop_program,		/* 0x18: usagefault */
-	NULL,				/* 0x1C */
-	NULL,				/* 0x20 */
-	NULL,				/* 0x24 */
-	NULL,				/* 0x28 */
-	&__stop_program,		/* 0x2C: svc */
-	&__stop_program,		/* 0x30: debugmon */
-	NULL,				/* 0x34 */
-	&__stop_program,		/* 0x38: pendsv */
-	&__stop_program,		/* 0x3C: systick */
-	&__stop_program,		/* 0x40: powerclock */
-	&__stop_program,		/* 0x44: radio */
-	&__stop_program,		/* 0x48: uarte0uart0 */
-	&__stop_program,		/* 0x4C: spim0spis0twim0twis0spi0twi0 */
-	&__stop_program,		/* 0x50: spim1spis1twim1twis1spi1twi1 */
-	&__stop_program,		/* 0x54: nfct */
-	&__stop_program,		/* 0x58: gpiote */
-	&__stop_program,		/* 0x5C: saadc */
-	&__stop_program,		/* 0x60: timer0 */
-	&__stop_program,		/* 0x64: timer1 */
-	&__stop_program,		/* 0x68: timer2 */
-	&__stop_program,		/* 0x6C: rtc0 */
-	&__stop_program,		/* 0x70: temp */
-	&__stop_program,		/* 0x74: srng */
+	&__reset_handler,		/* 0x04 -15 ARM Reset */
+	&__stop_program,		/* 0x08 -14 ARM NonMaskableInt */
+	&__stop_program,		/* 0x0C -13 ARM HardFault */
+	&__stop_program,		/* 0x10 -12 ARM MemoryManagement */
+	&__stop_program,		/* 0x14 -11 ARM BusFault */
+	&__stop_program,		/* 0x18 -10 ARM UsageFault */
+	&__stop_program,		/* 0x1C -9 ARM SecureFault */
+	&__stop_program,		/* 0x20 -8 Reserved */
+	&__stop_program,		/* 0x24 -7 Reserved */
+	&__stop_program,		/* 0x28 -6 Reserved */
+	&__stop_program,		/* 0x2C -5 ARM SVCall */
+	&__stop_program,		/* 0x30 -4 ARM DebugMonitor */
+	&__stop_program,		/* 0x34 -2 ARM PendSV */
+	&__stop_program,		/* 0x38 -1 ARM SysTick */
+	&__stop_program,		/* 0x3C #0 PM */
+	&__stop_program,		/* 0x40 #1 SYSCTRL */
+	&__stop_program,		/* 0x44 #2 WDT */
+	&__stop_program,		/* 0x48 #3 RTC */
+	&__stop_program,		/* 0x4C #4 EIC */
+	&__stop_program,		/* 0x50 #5 NVMCTRL */
+	&__stop_program,		/* 0x54 #6 DMAC */
+	&__stop_program,		/* 0x58 #7 USB */
+	&__stop_program,		/* 0x5C #8 EVSYS */
+	&__stop_program,		/* 0x60 #9 SERCOM0 */
+	&__stop_program,		/* 0x64 #10 SERCOM1 */
+	&__stop_program,		/* 0x68 #11 SERCOM2 */
+	&__stop_program,		/* 0x6C #12 SERCOM3 */
+	&__stop_program,		/* 0x70 #13 SERCOM4 */
+	&__stop_program,		/* 0x74 #14 SERCOM5 */
+	&__stop_program,		/* 0x78 #15 TCC0 */
+	&__stop_program,		/* 0x7C #16 TCC1 */
+	&__stop_program,		/* 0x80 #17 TCC2 */
+	&__stop_program,		/* 0x84 #18 TC3 */
+	&__stop_program,		/* 0x88 #19 TC4 */
+	&__stop_program,		/* 0x8C #20 TC5 */
+	&__stop_program,		/* 0x90 #21 TC6 */
+	&__stop_program,		/* 0x94 #22 TC7 */
+	&__stop_program,		/* 0x98 #23 ADC */
+	&__stop_program,		/* 0x9C #24 AC */
+	&__stop_program,		/* 0xA0 #25 DAC */
+	&__stop_program,		/* 0xA4 #26 PTC */
+	&__stop_program,		/* 0xA8 #27 I2S */
+	&__stop_program,		/* 0xAC #28 AC1 */
+	&__stop_program,		/* 0xB0 #29 TCC3 */
 };
 
 void
